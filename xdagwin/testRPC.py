c3b4a4bb1c38cbef8e76774ffe806b1ca515fd5c
hillhero789 <hillhero789@163.com>
2018-11-21 10:28:47 +0800

    tes JSON RPC

--- xdagwin/testRPC.py
+++ xdagwin/testRPC.py
@@ -3,7 +3,10 @@
 import requests
 import json
 
-url = 'http://pool.xdag.us:7667' # The RPC server url
-body = {"method":"xdag_state", "params":[], "id":1}
+#url = 'http://pool.xdag.us:7667' # The RPC server url
+#body = {"method":"xdag_state", "params":[], "id":1}
+
+url = 'http://127.0.0.1:8888'
+body = {"method":"xdag_version", "params":[], "id":1}
 resp = requests.post(url, data = json.dumps(body))
 print(resp.text)
