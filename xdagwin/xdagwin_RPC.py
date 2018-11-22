9aa2e23b526ffc5afacc64ba9a9e76ee01efd40a
hillhero789 <hillhero789@163.com>
2018-11-22 23:38:03 +0800

    more attempts

--- xdagwin/xdagwin_RPC.py
+++ xdagwin/xdagwin_RPC.py
@@ -23,22 +23,9 @@ allOutputTxs = []       #[wallet，哈希，数量，时间，...]
 newAllInputTxs = []     #获取最新输入交易
 newAllOutputTxs = []    #获取最新输出交易
 txsLatestDict = {'Input': '', 'Output': ''}        
-walletAddr = 'ovjaYrrxw/IuK7UHAWv5d9ByWCdQPTrS'
+WALLETADDR = 'ovjaYrrxw/IuK7UHAWv5d9ByWCdQPTrS'
 
-
-def getPageData(href,tryTimes = 2):#增加错误处理，连接错误时可尝试多次，最终失败则返回空字符串
-        i = 0
-        res = None
-        for i in range(0, tryTimes):
-                try:
-                        res = requests.get(href)
-                        break
-                except requests.exceptions.ConnectionError:
-                        print('Get ' + href + ' data error for ' + str(i+1) + ' time.')
-                        res = None
-        return res
-
-def getXdagRpcJson(url, body, attemptTimes = 5):
+def getXdagRpcJson(url, body, attemptTimes = 10):
     errorCounter = 0
     connError = 0
     while True:
@@ -53,10 +40,12 @@ def getXdagRpcJson(url, body, attemptTimes = 5):
                 if errorCounter >= attemptTimes:
                     print(str(datetime.datetime.now()) +' get data ERROR!')
                     return None
+                time.sleep(3)
                 continue
         except requests.exceptions.ConnectionError:
             connError += 1
             print(str(datetime.datetime.now()) +' Connection error for '+ str(connError) +' times!')
+            time.sleep(3)
             if connError >= attemptTimes:
                 print(str(datetime.datetime.now()) +' conn ERROR!')
                 return None
@@ -143,7 +132,7 @@ def getMatchAndUnmatchBet(paraInputTxs, paraMatchBet, paraUnmatchBet):#paraInput
                         paraMatchBet.append(tmpStr)
                 i -= 4
 
-def reward(paraOutputTxs, paraMatchBet):#获取所有output交易，判断是否已转入到matchBet对应的地址，如果是，则已完成，否则未完成，进行转账
+def reward(paraOutputTxs, paraMatchBet, paraUnMatchBet):#获取所有output交易，判断是否已转入到matchBet对应的地址，如果是，则已完成，否则未完成，进行转账
         if paraMatchBet == []:
                 return
         i = 0
@@ -165,18 +154,32 @@ def reward(paraOutputTxs, paraMatchBet):#获取所有output交易，判断是否
                                         else:
                                                 k = tmpOutputTxs.index(tmpWalletAddr, k+1)#如果钱包地址一致，金额不一致，则继续向后查找，找不到了，则转账        
                         except ValueError:
-                                doXfer(tmpWalletAddr, float(paraMatchBet[i+2])*2*(1-fee))
+                                doXfer(tmpWalletAddr, float(paraMatchBet[i+2])*2*(1-fee), paraUnMatchBet)
 
-def doXfer(walletAddr, ammount):        #向胜利者发送XDAG       成功返回交易哈希，失败返回None
+def doXfer(walletAddr, ammount, unmatchBet):        #向胜利者发送XDAG       成功返回交易哈希，失败返回None
+        #balance = 0.0
+        #i = 0
+        #for i in range(0,len(unmatchBet),3):
+        #        balance += float(unmatchBet[i+2])
+        #balance += ammount
+
+        #url = 'http://pool.xdag.us:7667'
+        #body = {"method":"xdag_get_balance", "params":[ WALLETADDR ], "id":1}
+        #resultJson = getXdagRpcJson(url, body)
+        #if resultJson is not None:
+                #if '%.9f'%(balance) == resultJson['result'][0]['balance']:
         url = 'http://127.0.0.1:8888'
-        body = {"method":"xdag_do_xfer", "params":[{"amount":str('%.9f'%(ammount)), "address":walletAddr, "remark":"REMARK"}], "id":1}
+        body = {"method":"xdag_do_xfer", "params":[{"amount":'%.9f'%(ammount), "address":walletAddr, "remark":"REMARK"}], "id":1}
         resultJson = getXdagRpcJson(url, body)
         if resultJson is not None:
-                print(str(datetime.datetime.now()) + ' xfer ' +str('%.9f'%(ammount))+' to '+ walletAddr +' succesfully!')
+                print(str(datetime.datetime.now()) + ' xfer ' +'%.9f'%(ammount)+' to '+ walletAddr +' succesfully!')
                 return resultJson['result'][0]['block']
         else:
-                print(str(datetime.datetime.now()) + ' xfer ERROR: Need to xfer ' +str('%.9f'%(ammount))+' to '+ walletAddr +'!')
+                print(str(datetime.datetime.now()) + ' xfer ERROR: Need to xfer ' +'%.9f'%(ammount)+' to '+ walletAddr +'!')
                 return None
+                #else:
+                #        print(str(datetime.datetime.now()) + ' balance not match ERROR!')
+                #        return None
 
 
 def calTxVal(paraTxHash):#计算传输哈希值
@@ -195,7 +198,7 @@ def refreshPage(paraUnmatchBet, paraMatchBet):
 
         for i in range(len(paraMatchBet)-4, max(0,len(paraMatchBet)-44)-1,-4):
                 if paraMatchBet[i+3] == 'winner':
-                        tdHtml = r'<td class = "STYLE1">Win '+ str('%.9f'%(float(paraMatchBet[i+2])*2*(1-fee))) +r' XDAG</td></tr>'
+                        tdHtml = r'<td class = "STYLE1">Win '+ '%.9f'%(float(paraMatchBet[i+2])*2*(1-fee)) +r' XDAG</td></tr>'
                 else:
                         tdHtml = r'<td>lose</td></tr>'
                 matchBetTalbeBody = matchBetTalbeBody + r'<tr><td>' + paraMatchBet[i] + r'</td><td>' + paraMatchBet[i+1] + r'</td><td>' + str(calTxVal(paraMatchBet[i+1])) + r'</td><td>' + paraMatchBet[i+2] + r'</td>'+ tdHtml
@@ -212,8 +215,8 @@ def refreshPage(paraUnmatchBet, paraMatchBet):
 
 #以下代码用于确认当前区块浏览器中记录的游戏已经清空
 while True:     #获取所有交易数据
-        getAllTxs(allInputTxs, allOutputTxs, walletAddr) 
-        getLatestTx(txsLatestDict,walletAddr)
+        getAllTxs(allInputTxs, allOutputTxs, WALLETADDR) 
+        getLatestTx(txsLatestDict,WALLETADDR)
         if allInputTxs == []:   #空表示无交易，继续等待
                 time.sleep(60)
                 continue
@@ -221,7 +224,7 @@ while True:     #获取所有交易数据
                 break
 
 getMatchAndUnmatchBet(allInputTxs,matchBet,unmatchBet)      #将匹配与未匹配交易进行记录
-reward(allOutputTxs,matchBet)
+reward(allOutputTxs,matchBet,unmatchBet)
 refreshPage(unmatchBet, matchBet)
 
 
@@ -234,8 +237,8 @@ while True:
         while True:
                 del(newAllInputTxs[:])  #清空
                 del(newAllOutputTxs[:]) #清空
-                getAllTxs(newAllInputTxs, newAllOutputTxs, walletAddr)
-                getLatestTx(txsLatestDict, walletAddr)
+                getAllTxs(newAllInputTxs, newAllOutputTxs, WALLETADDR)
+                getLatestTx(txsLatestDict, WALLETADDR)
                 if newAllInputTxs == []:   #空表示无交易，继续等待
                         continue
                 if txsLatestDict['Input'] == newAllInputTxs[1] and (newAllOutputTxs == [] or txsLatestDict['Output'] == newAllOutputTxs[1]):   #证明获取所有交易期间没有增加新的交易，如果不符合，则需重新获取所有交易
@@ -247,7 +250,7 @@ while True:
         else:
                 print(str(datetime.datetime.now()) + ' New input arrived!')
                 getMatchAndUnmatchBet(newAllInputTxs[ 0 : oldInputTxTopIndex - 1], newMatchBet, unmatchBet)      #将新增交易记录到匹配与未匹配交易列表，得到新的匹配列表
-                reward([], newMatchBet)                 #由于新的匹配交易，不可能有已经被支付过，所以reward第一个参数为空
+                reward([], newMatchBet,unmatchBet)                 #由于新的匹配交易，不可能有已经被支付过，所以reward第一个参数为空
                 for newMatchBetItem in newMatchBet:     #向matchBet列表增加新元素，但是只保留最近30个，新元素在后，老元素在前
                         matchBet.append(newMatchBetItem)
                 del(newMatchBet[:])
