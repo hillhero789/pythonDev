ac240a53154bde672fb872748227ce6c4fa97846
hillhero789 <hillhero789@163.com>
2019-01-15 13:55:07 +0800

    fix friends link

--- xdagwin/htmlCodes_bootstrap.py
+++ xdagwin/htmlCodes_bootstrap.py
@@ -98,7 +98,7 @@ tableHeader = """
 
 
 footer = """
-<div class="container text-center">友情链接：<a href="xdagpark.com">XDAG公园</a><br><br>Copyright 2018 xdagholder</div>
+<div class="container text-center">友情链接：<a href="https://xdagpark.com">XDAG公园</a><br><br>Copyright 2018 xdagholder</div>
 <script src="./sortTable.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
